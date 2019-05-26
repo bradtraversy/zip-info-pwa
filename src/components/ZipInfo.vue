3d4504a7afe7b187f2a7aed7c83c91277106b543
Brad Traversy <traversymedia@gmail.com>
2019-05-26 19:53:00 -0400

    ZipInfo component

---
+++ src/components/ZipInfo.vue
@@ -0,0 +1,39 @@
+<template>
+  <ion-card v-if="info">
+    <ion-card-header>
+      <ion-card-subtitle>{{info['post code']}}</ion-card-subtitle>
+      <ion-card-title>{{info['places'][0]['place name']}}</ion-card-title>
+    </ion-card-header>
+
+    <ion-card-content>
+      <ion-list>
+        <ion-item>
+          <ion-label>
+            <strong>State:</strong>
+            {{info['places'][0]['state']}} ({{info['places'][0]['state abbreviation']}})
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <strong>Latitude:</strong>
+            {{info['places'][0]['latitude']}}
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-label>
+            <strong>Longitude:</strong>
+            {{info['places'][0]['longitude']}}
+          </ion-label>
+        </ion-item>
+      </ion-list>
+    </ion-card-content>
+  </ion-card>
+</template>
+
+<script>
+export default {
+  name: "ZipInfo",
+  props: ["info"]
+};
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,6 +7,7 @@
     </ion-header>
     <ion-content class="ion-padding">
       <ZipSearch v-on:get-zip="getZipInfo"/>
+      <ZipInfo v-bind:info="info"/>
     </ion-content>
   </div>
 </template>
@@ -14,12 +15,31 @@
 
 <script>
 import ZipSearch from "../components/ZipSearch";
+import ZipInfo from "../components/ZipInfo";
 export default {
   name: "home",
-  components: { ZipSearch },
+  components: { ZipSearch, ZipInfo },
+  data() {
+    return {
+      info: null
+    };
+  },
   methods: {
-    getZipInfo(zip) {
-      console.log(zip);
+    async getZipInfo(zip) {
+      const res = await fetch(`https://api.zippopotam.us/us/${zip}`);
+      if (res.status == 404) {
+        this.showAlert();
+      }
+      this.info = await res.json();
+    },
+    showAlert() {
+      return this.$ionic.alertController
+        .create({
+          header: "Not Valid",
+          message: "Please enter a valid US zipcode",
+          buttons: ["OK"]
+        })
+        .then(a => a.present());
     }
   }
 };
